9620264d94a0026d67a1302d0c3c240a56354d0a
Al-Habib <109785052+Al-Habib18@users.noreply.github.com>
2023-01-20 22:24:31 +0600

    is for adding a file of word capitaization

---
+++ Word_Capitaization.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    char d= toupper(s[0]);
+    s[0] = d;
+
+    for(int i = 0; i<s.size(); i++){
+        cout << s[i];
+    }
+    return 0;
+}
