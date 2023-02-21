f1197ccfd1bbd1da3b6d7df05121aae4b06de7ea
Al-Habib <109785052+Al-Habib18@users.noreply.github.com>
2023-02-21 07:54:08 +0600

    Fox and Snake

--- str.cpp
+++ str.cpp
@@ -2,20 +2,50 @@
 using namespace std;
 
 int  main(){
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
 
-    string first,second, third;
-    cin >> first >> second >> third;
-
-    string s = first + second;
-
-    sort(s.begin(), s.end());
-    sort(third.begin(), third.end());
-
-    if( s == third){
-        cout << "YES";
-    }else{
-        cout << "NO";
+    string hash,f_hash, l_hash;
+    for(int i =0; i<m ;i++){
+        hash.push_back('#');
+    }
+    
+    for(int i=0; i<m ; i++){
+        if( i==0){
+            f_hash.push_back('#');
+        }
+        else{
+            f_hash.push_back('.');
+        }
     }
 
+    for(int i=0; i<m;i++){
+        if(i ==m-1){
+            l_hash.push_back('#');
+        }
+        else{
+            l_hash.push_back('.');
+        }
+    }
+    int l=1;
+    int f=3;
+    for(int i=0; i<n; i++){
+        if(i == 0 || i%2 == 0){
+            cout << hash << endl;
+        }
+        else{
+            if( l < f){ 
+            cout << l_hash << endl;
+            l +=4;
+            }
+            else if( f < l){
+            cout << f_hash << endl;
+            f += 4;
+            }
+  
+        }
+    }
 return 0;
 }
