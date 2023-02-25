ec57629e1596cf1cbba4ae3a29664ff929f3c0d0
Al-Habib <109785052+Al-Habib18@users.noreply.github.com>
2023-02-25 12:37:40 +0600

    Vasya the Hipster

--- new.cpp
+++ new.cpp
@@ -3,32 +3,24 @@ using namespace std;
 
 int main()
 {
-    int n, k, sum = 0,a = 0;
-    cin >> n >> k;
+    int a, b,diff= 0 , same = 0, rem;
+    cin >> a >> b;
 
-    vector<int> vi;
-    for(int i=0; i<=n; i++){
-        a = a + (5*i);
-        vi.push_back(a); // 0 5 15 30 50 75 105 140 180 225 275
+    if( a > b){
+        diff =b;
+        rem = a-b;
+        same = rem/2;
+    }
+    else if( b > a){
+        diff = a;
+        rem = b-a;
+        same = rem/2;
+    }
+    else{
+        diff = a;
+        same = 0;
     }
 
-    int min = 4*60 ;
-    int rem = min - k;
-    int problem;
-
-    for(int i=1; i<= n; i++){
-        if( vi[i] > min){
-            problem = i-1;
-            break;
-        }
-        else if(vi[i] > rem ){
-            problem = i-1;
-            break;
-        }
-        else{
-            problem = i;
-        }
-    }
-
-    cout << problem <<endl;
+    cout <<diff << " " << same << endl;
+    return 0;
 }
